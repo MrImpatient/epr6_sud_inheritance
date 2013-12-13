6fd091cd5f2ef5011f594854e8740df89f9ffb43
MrImpatiens <impatiensfan@gmail.com>
2013-12-13 15:56:40 +0100

    note - Befehl implementiert:
    note b1 578 (alternativ: note b1 5 dann note b1 7  ...) fügt Notiz hinzu mit Zahlen die legal in Feld passen.
    
    note b1  ruft Notizzettel ab
    
    notes - Befehl implementiert:
    
    Gibt man notes ein, dann werden automatisch alle Notizzettel generiert.
    
    TODO: eventuell Löschfunktion einbauen für Notizzettel

--- sudoku_extended.py
+++ sudoku_extended.py
@@ -5,6 +5,12 @@ if __name__ == '__main__':
     ## Workaround 4 Pickleing nested classes
     class Item(Sudoku.Item):
         fixit = False
+        note = ""
+        def getnote(self):
+            print(self.note)
+            getch()
+        def setnote(self,s:str):
+            self.note += s
         
     #Neue Klasse Sudoku2
     class Sudoku2(Sudoku, Item):
@@ -41,17 +47,39 @@ if __name__ == '__main__':
             row, col = self._mapper(row, col)
             self[row][col] = self.get_empty()
         
-        #Aufruf z. B. mit note b1 "hallo"
+        #Aufruf z. B. mit note b1
         def get_note(self, row:str, col:str):
-            print("Hier get_note")
-            getch()
+            self.grid = self.get(row, col)
+            #print(self.grid.note)
+            self.grid.getnote()
+            #def set(self, row:str, col:str, value:int)
             
-        #Aufruf z. B. mit note b1 dies_ist_ein_text
+        #Aufruf z. B. mit note b1 157
         def set_note(self, row:str, col:str, hint:str):
-            print("Hier set_note")
-            print(hint)
+            self.grid = self.get(row, col)
+            #self.grid.note = str(hint)
+            self.grid.setnote(hint)
+            print("Notiz gesetzt!")
             getch()
-        pass
+        
 
-    #Jetzt erbt Sudoku wieder von dieser neuen Klasse...
+        def generate_notes(self):
+            print("generate notes")
+            for value in range (1,9):
+                for col in range(self._size):
+                      for row in range(self._size):
+                          if self[row][col].get()== 0:
+                              self[row][col].set(value)
+                              if self.is_valid_row(row) or self.is_valid_col(col) or self.is_valid_submarix(row, col):
+                                 self[row][col].set(0)
+                              else:
+                                 #append number at hint
+                                 self[row][col].note += str(value)
+                                 self[row][col].set(0)
+                                  
+
+    #Hier wird das Objekt der neuen Item-Klasse dem Konstuktor
+    #__init__(Grid.Item) übergeben und der mainloop gestartet
+    #Letzterer Konstruktor übergibt das Item-Objekt an den Grid
+    #Konstruktor
     Sudoku2(Item).mainloop()
