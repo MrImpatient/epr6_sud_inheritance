c6b7d02d78ade0e9cfea763f573609a4e3529c11
MrImpatiens <impatiensfan@gmail.com>
2013-12-16 18:52:59 +0100

    level implementiert (0 - 10)

--- sudoku_extended.py
+++ sudoku_extended.py
@@ -151,8 +151,60 @@ if __name__ == '__main__':
                             print("Wert gesetzt")
                             return 1
 
-           
+        def level(self):
+            cl = [0]*9
+            emptyfields = 0
+            #Calculate notes
+            self.printflag = False
+            self.generate_notes()
+            self.printflag = True
+            #Counting 1er, 2er, 3er, 4er, 5er,... choices
+            for col in range(self._size):
+                for row in range(self._size):
+                    if self[row][col].get()== 0:
+                        emptyfields +=1
+                        if len(self[row][col].note) == 1:
+                            cl[0] += 1
+                        if len(self[row][col].note) == 2:
+                            cl[1] += 1
+                        if len(self[row][col].note) == 3:
+                            cl[2] += 1
+                        if len(self[row][col].note) == 4:
+                            cl[3] += 1
+                        if len(self[row][col].note) == 5:
+                            cl[4] += 1
+                        if len(self[row][col].note) == 6:
+                            cl[5] += 1
+                        if len(self[row][col].note) == 7:
+                            cl[6] += 1
+                        if len(self[row][col].note) == 8:
+                            cl[7] += 1
+                        if len(self[row][col].note) == 9:
+                            cl[8] += 1
+            #calculating set fields 
+            fields = 81-emptyfields
+            if fields == 81:
+                print("Sudoku gelöst!")
+                getch()
+                return
+            #1er-choices count as filled fields
+            givenfields = fields + cl[0]
+            fieldindex = -1*givenfields/81 + 1 #range 1 to 0 (= solved)
+            weightedchoices= cl[1]*2+cl[2]*3+cl[3]*4+cl[4]*5+cl[5]*6+cl[5]*6+cl[6]*7+cl[7]*8+cl[8]*9
+            choicesindex = weightedchoices/(81*9) #== 1 if all fields 9er-choices, 0 if S. solved
+
+            #temp = (fieldindex + choicesindex)/2
+            temp = choicesindex
+
+            levelvalue = 10 - (10 * temp)
+
+            levelvalue = round(levelvalue, 2)
             
+            print("\nlevel = ", levelvalue)
+            print("\n0/1/2/3 schwer")
+            print("4/5/6/7 mittel")
+            print("8/9/10 einfach")
+            getch()
 
     #Hier wird das Objekt der neuen Item-Klasse dem Konstuktor
     #__init__(Grid.Item) übergeben und der mainloop gestartet
